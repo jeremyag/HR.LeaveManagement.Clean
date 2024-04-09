577b2e13db58d8b379e097f823f902b99a730692
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2024-04-09 18:00:06 +0800

    Created Application Project and Added Contracts and Added GetAllLeaveTypes Handle.

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.MappingProfiles;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveAllocationRepository _leaveRepository;
+
+    public GetLeaveTypesQueryHandler(
+        IMapper mapper, ILeaveAllocationRepository leaveRepository
+        )
+    {
+        _mapper = mapper;
+        _leaveRepository = leaveRepository;
+    }
+    public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        // Query the database
+        var leaveTypes = await _leaveRepository.GetAsync();
+
+        // Map to DTOs
+        var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+        // return
+        return data;
+    }
+}
