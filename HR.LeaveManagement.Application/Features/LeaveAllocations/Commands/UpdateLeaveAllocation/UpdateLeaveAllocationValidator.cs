e43b38cfddc1cf03486238b4e84c8cb6ce2d15ae
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2024-04-10 20:19:19 +0800

    Created LeaveAllocation Controller.

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocations/Commands/UpdateLeaveAllocation/UpdateLeaveAllocationValidator.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Commands.UpdateLeaveAllocation;
+
+public class UpdateLeaveAllocationValidator : AbstractValidator<UpdateLeaveAllocationCommand>
+{
+    private readonly ILeaveTypeRepository _repo;
+    private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+
+    public UpdateLeaveAllocationValidator(ILeaveTypeRepository repo, ILeaveAllocationRepository leaveAllocationRepository)
+    {
+        this._repo = repo;
+        this._leaveAllocationRepository = leaveAllocationRepository;
+        RuleFor(p => p.NumberOfDays)
+            .GreaterThan(0).WithMessage("{PropertyName} must greater than {ComparisonValue}");
+
+        RuleFor(p => p.Period)
+            .GreaterThanOrEqualTo(DateTime.Now.Year)
+            .WithMessage("{PropertyName} must be after {ComparisonValue}");
+
+        RuleFor(p => p.LeaveTypeId)
+            .GreaterThan(0)
+            .MustAsync(LeaveTypeMustExist)
+            .WithMessage("{PropertyName} does not exist");
+
+        RuleFor(p => p.Id)
+            .NotNull()
+            .MustAsync(LeaveAllocationMustExist)
+            .WithMessage("{PropertyName} must be present");
+    }
+
+    private async Task<bool> LeaveAllocationMustExist(int id, CancellationToken token)
+    {
+        var leaveAllocation = await _leaveAllocationRepository.GetByIdAsync(id);
+        return leaveAllocation != null;
+    }
+
+    private async Task<bool> LeaveTypeMustExist(int id, CancellationToken token)
+    {
+        var leaveType = await _repo.GetByIdAsync(id);
+        return leaveType != null;
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveAllocations/Queries/GetLeaveAllocationDetails/GetLeaveAllocationDetailRequestHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Queries.GetLeaveAllocationDetails;
+
+public class GetLeaveAllocationDetailRequestHandler : IRequestHandler<GetLeaveAllocationDetailQuery, LeaveAllocationDetailsDto>
+{
+    private readonly ILeaveAllocationRepository _repo;
+    private readonly IMapper _mapper;
+
+    public GetLeaveAllocationDetailRequestHandler(ILeaveAllocationRepository repo, IMapper mapper)
+    {
+        this._repo = repo;
+        this._mapper = mapper;
+    }
+    public async Task<LeaveAllocationDetailsDto> Handle(GetLeaveAllocationDetailQuery request, CancellationToken cancellationToken)
+    {
+        var leaveAllocation = await _repo.GetLeaveAllocationWithDetails(request.Id);
+
+        return _mapper.Map<LeaveAllocationDetailsDto>(leaveAllocation);
+    }
+}
