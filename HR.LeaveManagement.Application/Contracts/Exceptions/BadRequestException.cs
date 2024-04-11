ef4af2e393f8d8ed2aca9dc46b82987ebd585c9b
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2024-04-11 10:30:45 +0800

    Created LeaveRequest Endpoinds

--- HR.LeaveManagement.Application/Contracts/Exceptions/BadRequestException.cs
+++ HR.LeaveManagement.Application/Contracts/Exceptions/BadRequestException.cs
@@ -13,7 +13,7 @@ public class BadRequestException : Exception
     public BadRequestException(string message, ValidationResult validationResult) : base(message)
     {
         ValidationErrors = new();
-        foreach(var error in validationResult.Errors)
+        foreach (var error in validationResult.Errors)
         {
             ValidationErrors.Add(error.ErrorMessage);
         }
